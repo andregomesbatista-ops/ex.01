d99eb617b55e3df2b7f85965a3550a9e787d08c0
André pyetro <andre.gomes.batista@escola.pr.gov.br>
2026-07-06 12:47:32 -0300

    Add files via upload

---
+++ ex.02/script.js
@@ -0,0 +1,18 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+
+    document.getElementById("mensagem").textContent = 
+    "modo claro ativado";
+}
+
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  
+  
+  document.getElementById("mensagem").textContent = 
+  "modo escuro ativado";
+}
